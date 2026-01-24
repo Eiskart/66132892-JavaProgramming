06a3d592c0a9e2fc319d6fcc0dbeec578449c82a
Eiskart <vlhphuc090506.nvtroi2124@gmail.com>
2026-01-24 22:01:02 +0700

    tạo class XinChao trong project HelloWorld

---
+++ HelloWorld/src/tiil/edu/XinChao.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package tiil.edu;
+
+/**
+ * 
+ */
+public class XinChao {
+
+	public static void main(String[] args) {
+		System.out.println("Xin Chao Java");
+		
+		System.out.println("5+3="+ (5+3));
+
+	}
+
+}

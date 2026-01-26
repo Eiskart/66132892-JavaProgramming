46309956cfb4c16ce1d4526bc7c210042d325e47
Eiskart <vlhphuc090506.nvtroi2124@gmail.com>
2026-01-26 21:00:00 +0700

    Merge branch 'main' of https://github.com/Eiskart/66132892-JavaProgramming.git into main

--- HelloWorld/src/tiil/edu/XinChao.java
+++ HelloWorld/src/tiil/edu/XinChao.java
@@ -1,5 +1,5 @@
 /**
- * 
+ 
  */
 package tiil.edu;
 
@@ -11,8 +11,27 @@ public class XinChao {
 	public static void main(String[] args) {
 		System.out.println("Xin Chao Java, Tôi đang học lập trình");
 		
-		System.out.println("Em là Võ Lê Hoàng Phúc, sinh viên đại học Nha Trang ");
-
+		System.out.println("5+3="+ (5+3));
+		System.out.println("5-3="+ (5-3));
+		System.out.println("5*3="+ (5*3));
+		System.out.println("5/3="+ (5/3));
+		
+		String text = """
+			======================
+            HELLO, MODERN JAVA!
+            ======================
+            by Mai Cường Thọ NTU
+            ======================
+				""";
+		System.out.println(text);
+		
+		var so = 5;
+		var cau = "Xin chao";
+		var list = java.util.List.of("Java", "code","NTU");
+		System.out.println("so:"+ so);
+		System.out.println("cau:"+ cau);
+		System.out.println("list:"+ list);
+		
 	}
 
 }

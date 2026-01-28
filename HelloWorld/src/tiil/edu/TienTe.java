6882b379c5b5c7fc0673593c970c4ad7c3257b52
Eiskart <vlhphuc090506.nvtroi2124@gmail.com>
2026-01-28 21:04:43 +0700

    bt2 cua bai 2

---
+++ HelloWorld/src/tiil/edu/TienTe.java
@@ -0,0 +1,13 @@
+package tiil.edu;
+
+public class TienTe {
+
+	public static void main(String[] args) {
+		int tienDo=20;
+		int tienEuro=10;
+		System.out.println(tienDo+"$="+(tienDo*23500)+"VND");
+		System.out.println(tienEuro+"Euro="+(tienEuro*27000)+"VND");
+
+	}
+
+}

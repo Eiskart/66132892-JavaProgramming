09bdbd4958ce0ae7a2051417f97b64dc6bb29b70
PhucVo <153920954+Eiskart@users.noreply.github.com>
2026-03-19 09:05:59 +0700

    viết lớp test nhân viên

---
+++ ViDuOOP_QLNV/src/vlhp/QLNV/TestRun.java
@@ -0,0 +1,10 @@
+package vlhp.QLNV;
+
+public class TestRun {
+
+	public static void main(String[] args) {
+		NhanVien nv1 = new NhanVien("001","Mai Cường Thọ",2300000);
+		System.out.println(nv1.toString());
+	}
+
+}

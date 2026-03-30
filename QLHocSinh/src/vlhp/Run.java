c4aec3c2296a16ea82134edaf9bbdc97bda21baf
PhucVo <153920954+Eiskart@users.noreply.github.com>
2026-03-30 20:47:56 +0700

    Thêm danh sách học sinh

--- QLHocSinh/src/vlhp/Run.java
+++ QLHocSinh/src/vlhp/Run.java
@@ -1,5 +1,8 @@
 package vlhp;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Run {
 
 	public static void main(String[] args) {
@@ -13,7 +16,37 @@ public class Run {
 		
 		System.out.println(hs1.toString());
 		System.out.println(hs2.toString());
+		
+		HocSinh hs3 = new HocSinh();
+		Scanner banPhim = new Scanner(System.in);
+		System.out.print("Nhap ten cua hoc sinh thu 3:");
+		hs3.setTenHS(banPhim.nextLine());
+		System.out.print("Nhap tuoi cua hoc sinh thu 3:");
+		hs3.setTuoiHS(banPhim.nextInt());
+		System.out.print("Nhap lop cua hoc sinh thu 3:");
+		hs3.setLopHS(banPhim.nextLine());
+		
+		System.out.println(hs3.toString());
+		
+		
+		ArrayList<HocSinh> dsHS = new ArrayList<HocSinh>();
+		System.out.print("Nhap n cua danh sach hoc sinh:");
 
+		int n = banPhim.nextInt();
+		
+		for(int i = 0;i < n;i++) {
+			HocSinh hsI = new HocSinh();
+			System.out.printf("Nhap ten cua hoc sinh thu %d:",i);
+			hsI.setTenHS(banPhim.nextLine());
+			System.out.printf("Nhap tuoi cua hoc sinh thu %d:",i);
+			hsI.setTuoiHS(banPhim.nextInt());
+			System.out.printf("Nhap lop cua hoc sinh thu %d:",i);
+			hsI.setLopHS(banPhim.nextLine());
+			dsHS.add(hsI);
+
+		}
+		
+		
 		
 	}
 

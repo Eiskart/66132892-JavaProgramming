1071bf29c88cadc7990dedf0cb02b7573218b50a
PhucVo <153920954+Eiskart@users.noreply.github.com>
2026-04-04 19:42:32 +0700

    thêm lớp máy tinh bảng và laptop kế thừa các chức năng phù hợp, phần thân code còn trống vì không có yêu cầu cụ thể

---
+++ TruuTuong_HeThongThietBiDienTu/src/vlhp/MayTinhBang.java
@@ -0,0 +1,41 @@
+package vlhp;
+
+public class MayTinhBang extends ThietBiDienTu implements CoTheCamUng, CoTheKetNoiInternet {
+
+	@Override
+	public void ketNoiWifi() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void ngatKetNoi() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void cham() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void vuot() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void bat() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void tat() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}

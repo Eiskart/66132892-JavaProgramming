500ca20675318ce7725c49b794930c2c4d9e84a1
PhucVo <153920954+Eiskart@users.noreply.github.com>
2026-04-11 23:04:07 +0700

    đọc dữ liệu từ baitho.txt

--- ViDuStream/src/vlhp/ViDuDocStream.java
+++ ViDuStream/src/vlhp/ViDuDocStream.java
@@ -1,6 +1,7 @@
 package vlhp;
 
 import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
@@ -8,12 +9,14 @@ public class ViDuDocStream {
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-		InputStreamReader ipRead = new InputStreamReader(System.in);
+		FileInputStream file = new FileInputStream("baitho.txt");
+		InputStreamReader ipRead = new InputStreamReader(file);
 		BufferedReader bufRead = new BufferedReader(ipRead);
-		System.out.print("Nhap chuoi:");
-		String doc = bufRead.readLine();
-		System.out.println(doc);
-		System.out.print("test");
+		String line1 = bufRead.readLine();
+		System.out.println(line1);
+		String line2 = bufRead.readLine();
+		System.out.println(line2);
+		bufRead.close();
 	}
 
 }

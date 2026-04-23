810668892f54b9b8227245d44ba14b6c37a9bd0f
PhucVo <153920954+Eiskart@users.noreply.github.com>
2026-04-23 07:36:11 +0700

    LẦN THỨ 1

---
+++ ThiGK_66132892/src/thigk2/VoLeHoangPhuc/Cau1.java
@@ -0,0 +1,25 @@
+package thigk2.VoLeHoangPhuc;
+
+import java.util.*;
+
+public class Cau1 {
+	
+	/* sử dụng lớp */
+	
+	public static void main() {
+		double weight;
+		double height;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("nhap can nang theo kg:");
+		weight = sc.nextDouble();
+		sc.nextLine();
+		System.out.print("nhap chieu cao theo met:");
+		height = sc.nextDouble();
+		sc.nextLine();
+		double BMI = weight/(height*Math.PI);
+		System.out.println("Chi so BMI cua ban la:"+BMI);
+		if(BMI < )
+	}
+}

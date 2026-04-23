0d8b9c087cfcf785cd6577a997790ef5edc36b8e
PhucVo <153920954+Eiskart@users.noreply.github.com>
2026-04-23 08:27:53 +0700

    LẦN THỨ 6

--- ThiGK_66132892/src/thigk2/VoLeHoangPhuc/Cau3_DocFile.java
+++ ThiGK_66132892/src/thigk2/VoLeHoangPhuc/Cau3_DocFile.java
@@ -4,19 +4,29 @@ import java.io.*;
 import java.util.*;
 
 public class Cau3_DocFile {
-
-	public static void main(String[] args) throws FileNotFoundException {
+	/* tạo SoNguyen.txt trong folder dự án, dùng FileReader để dọc folder và BufferedReader để bọc
+	 , dùng boolean flag để kiểm tra X( X = 2) có nằm trong danh sách số hay không */
+	public static void main(String[] args) throws NumberFormatException, IOException {
 		
 		String text = "SoNguyen.txt";
 		List<Integer> so = new ArrayList<>();
 		
 		BufferedReader bf = new BufferedReader(new FileReader(text));
 		String dong;
-		while (dong = bf.readLine() != null) {
-			int num
+		while ((dong = bf.readLine()) != null) {
+			so.add(Integer.parseInt(dong.trim()));
 		}
+		System.out.println("Danh sach so nguyen la:");
+		boolean flag = false;
 		
-
+		for(Integer x : so) {
+			System.out.println(x);
+			if(x == 2) flag = true;
+		}
+		if(flag == true)
+		System.out.println("Gia tri X co trong danh sach");
+		else System.out.println("Gia tri X khong co trong danh sach");
+		bf.close();
 	}
 
 }

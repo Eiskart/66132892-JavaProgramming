20e3e78b20f6446d94445ce027654703915813ff
PhucVo <153920954+Eiskart@users.noreply.github.com>
2026-04-23 07:56:32 +0700

    LẦN THỨ 3

---
+++ ThiGK_66132892/src/thigk2/VoLeHoangPhuc/Cau2_MainSinhVien.java
@@ -0,0 +1,22 @@
+package thigk2.VoLeHoangPhuc;
+
+import java.util.*;
+
+public class Cau2_MainSinhVien {
+	/* */
+	public static void main() {
+		List<SinhVien> dsSV = new ArrayList<>();
+		SinhVien sv1 = new SinhVien("111","Nguyen Van A", 2000, "099432321");
+		SinhVien sv2 = new SinhVien("112","Nguyen Van B", 2002, "095267588");
+		SinhVien sv3 = new SinhVien("113","Nguyen Van C", 2005, "092532767");
+		dsSV.add(sv1);
+		dsSV.add(sv2);
+		dsSV.add(sv3);
+		System.out.println("Danh sach sinh vien la:");
+		for(int i= 0; i< dsSV.size();i++) {
+			System.out.println(dsSV.);
+		}
+	}
+	
+	
+}
--- ThiGK_66132892/src/thigk2/VoLeHoangPhuc/SinhVien.java
+++ ThiGK_66132892/src/thigk2/VoLeHoangPhuc/SinhVien.java
@@ -6,6 +6,48 @@ public class SinhVien {
 	protected int namSinh;
 	protected String sdt;
 	
+	public SinhVien(String mssv, String hoTen, int namSinh, String sdt) {
+		super();
+		this.mssv = mssv;
+		this.hoTen = hoTen;
+		this.namSinh = namSinh;
+		this.sdt = sdt;
+	}
 
+	public String getMssv() {
+		return mssv;
+	}
+
+	public void setMssv(String mssv) {
+		this.mssv = mssv;
+	}
+
+	public String getHoTen() {
+		return hoTen;
+	}
+
+	public void setHoTen(String hoTen) {
+		this.hoTen = hoTen;
+	}
+
+	public int getNamSinh() {
+		return namSinh;
+	}
+
+	public void setNamSinh(int namSinh) {
+		this.namSinh = namSinh;
+	}
+
+	public String getSdt() {
+		return sdt;
+	}
+
+	public void setSdt(String sdt) {
+		this.sdt = sdt;
+	}
+	
+	
+	
+	
 	
 }
